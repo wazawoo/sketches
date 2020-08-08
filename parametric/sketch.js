2a4ef17c4b05b6207f3292d4db3fa9cff21dbb22
wazawoo <reed.moseng@gmail.com>
2020-08-07 22:04:06 -0500

    Add p5.js version of sketch

---
+++ parametric/sketch.js
@@ -0,0 +1,49 @@
+//inspired by Alexander Miller: https://www.youtube.com/watch?v=LaarVR1AOvs
+
+var t = 0;
+
+var SPEED_MULTIPLIER = 1;
+
+function x1(t) {
+    return sin(t / 10) * 100;
+}
+
+function y1(t) {
+    return cos(t / 10) * 100;
+}
+
+function x2(t) {
+    return sin(t / 20) * 100;
+}
+
+function y2(t) {
+    return cos(t / 20) * 100;
+}
+
+var ww = 500;
+var hh = 500;
+
+function setup() {}
+    createCanvas(ww, hh)
+    background(40)
+}
+
+function draw() {
+    //draw translucent background so it slowly covers up the past
+    noStroke();
+    fill(255, 135, 0, 20);
+    rect(0, 0, width, height);
+
+    stroke(255);
+    strokeWeight(3);
+    translate(width / 2, height / 2);
+    for (var i = 0; i < SPEED_MULTIPLIER; i++) {
+        drawStuff();
+    }
+}
+
+function drawStuff() {
+    //draw a line between the individual points of two parametric functions
+    line(x1(t), y1(t), x2(t), y2(t));
+    t++;
+}
